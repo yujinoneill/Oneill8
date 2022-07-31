7fa6bd659efb7f4c41fb6734e3c5191d385ed792
Oneill <yujinoneill@gmail.com>
2022-07-31 21:23:27 +0900

    FEAT: Add minimun size of div, Pass the props to HomeMap

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,8 +24,8 @@ const Home = () => {
 
   return (
     <div className="container w-[90%] xl:max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-3 absolute top-14 bottom-0 left-0 right-0">
-      <div className="col-span-2">
-        <HomeMap height="95%" />
+      <div className="col-span-2 min-h-[30vh]">
+        <HomeMap height="95%" data={data} />
       </div>
       <div className="places col-span-1 mt-5 mb-3 mx-auto lg:mr-5 overflow-y-scroll">
         {data.map((item) => {
