da4e63d0bf8d03cb0fe2c8d3f409074240e35264
Oneill <yujinoneill@gmail.com>
2022-08-03 17:41:33 +0900

    FEAT: Link to review on My Page

--- client/src/pages/user/MyPage.jsx
+++ client/src/pages/user/MyPage.jsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import axios from "axios";
-import { useState } from "react";
 
 const MyPage = () => {
   const [reviews, setReviews] = useState();
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -14,9 +15,9 @@ const MyPage = () => {
   }, []);
 
   return (
-    <div className="container xl:max-w-5xl flex flex-col items-center mx-auto mt-6">
+    <div className="container w-[90%] xl:max-w-5xl flex flex-col items-center mx-auto mt-6">
       <h1 className="font-bold text-2xl mb-5">내가 쓴 리뷰 모아보기</h1>
-      {reviews &&
+      {reviews && reviews.length > 0 ? (
         reviews.map((el) => {
           return (
             <div className="card w-96 bg-base-100 shadow-xl mb-4" key={el._id}>
@@ -38,10 +39,25 @@ const MyPage = () => {
                 </div>
                 <span className="text-xs text-base-300">{el.date}</span>
                 <p>{el.review}</p>
+                <div class="card-actions justify-end">
+                  <button
+                    class="btn btn-sm btn-primary"
+                    onClick={() => navigate(`/place/${el.place}`)}
+                  >
+                    보러 가기
+                  </button>
+                </div>
               </div>
             </div>
           );
-        })}
+        })
+      ) : (
+        <p className="text-center">
+          리뷰를 남기지 않으셨네요!
+          <br />
+          방문한 맛집이 있다면 의견을 남겨 보시는 건 어떨까요?
+        </p>
+      )}
     </div>
   );
 };
